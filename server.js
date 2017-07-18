9e9e9ebea7c0ff02192f63d2ad30f592b27ae56d
Bryan <bryanmierke-macbookair@Bryans-MacBook-Air.local>
2017-07-18 15:16:06 -0700

    added sprint 2 solutin for server.js

--- server.js
+++ server.js
@@ -1,148 +1,108 @@
 // server.js
 // SERVER-SIDE JAVASCRIPT
 
-
-/////////////////////////////
-//  SETUP and CONFIGURATION
-/////////////////////////////
-
 //require express in our app
-let express = require('express'),
+var express = require('express'),
   bodyParser = require('body-parser'),
   db = require('./models');
 
 // generate a new express app and call it 'app'
-let app = express();
+var app = express();
 
 // serve static files in public
 app.use(express.static('public'));
 
 // body parser config to accept our datatypes
-app.use(bodyParser.urlencoded({
-  extended: true
-}));
-
-
-
-////////////////////
-//  Seed DATA
-///////////////////
-
-// let newBookUUID = 18; <-- WHAT DOES THIS DO????
-
-////////////////////
-//  ROUTES
-///////////////////
+app.use(bodyParser.urlencoded({ extended: true }));
 
 // define a root route: localhost:3000/
-app.get('/', function(req, res) {
-  res.sendFile('views/index.html', {
-    root: __dirname
-  });
+app.get('/', function (req, res) {
+  res.sendFile('views/index.html' , { root : __dirname});
 });
 
 // get all books
-app.get('/api/books', function(req, res) {
-  // send all books as JSON response
-  db.Book.find()
-    // populate fills in the author id with all the author data
+app.get('/api/books', function (req, res) {
+  // find one book by its id
+  db.Book.find({})
     .populate('author')
     .exec(function(err, books){
-      if (err) { return console.log("index error: " + err); }
+      if (err) {
+        res.status(500).send(err);
+        return;
+      }
       res.json(books);
     });
+
 });
 
-// get one book
-app.get('/api/books/:id', function(req, res) {
+
+
+app.get('/api/books/:id', function (req, res) {
   // find one book by its id
-  //console.log('books show', req.params.id);
-  let myBookId = req.params.id;
-  console.log("User submitted Book ID is "+myBookId);
-
-  // this takes the ID parameter from browser and uses it to return the book along with a populated author id
-  db.Book.findOne({_id: myBookId}).populate('author').exec(function(err, theBook) {
-    if(err){
-      console.log("error!", err);
-    }
-    res.json(theBook);
-  });
-
+  db.Book.findById(req.params.id)
+    // populate the author
+    .populate('author')
+    .exec(function(err, book){
+      if (err) {
+        res.status(500).send(err);
+        return;
+      }
+      res.json(book);
+    });
 
 });
 
-// app.get('/api/books/search', function(req,res){
-//   let searchTitleQuery = req.query.title || "";
-//   let searchAutherQuery = req.query.author || "";
-//   //sample url: /api/books/search?title="Mice"&author="Steinbeck"
-//
-//
-// })
 
-// create new book
-app.post('/api/books', function(req, res) {
+
+app.post('/api/books', function (req, res) {
   // create new book with form data (`req.body`)
   var newBook = new db.Book({
     title: req.body.title,
     image: req.body.image,
     releaseDate: req.body.releaseDate,
   });
-
-  // this code will only add an author to a book if the author already exists
+  // find the author from req.body
   db.Author.findOne({name: req.body.author}, function(err, author){
-    if (err){
+    if (err) {
       return console.log(err);
     }
-    newBook.author = author;
-    // add newBook to database
-    newBook.save(function(err, book){
-      if (err) {
-        return console.log("create error: " + err);
-      }
-      console.log("created ", book.title);
-      res.json(book);
-    });
+    // if that author doesn't exist yet, create a new one
+    if (author === null) {
+      db.Author.create({name:req.body.author, alive:true}, function(err, newAuthor) {
+        createBookWithAuthorAndRespondTo(newBook, newAuthor, res);
+      });
+    } else {
+      createBookWithAuthorAndRespondTo(newBook, author, res);
+    }
   });
-
-
 });
 
-// update book
-app.put('/api/books/:id', function(req, res) {
-  // get book id from url params (`req.params`)
+function createBookWithAuthorAndRespondTo(book, author, res) {
+  // add this author to the book
+  book.author = author;
+  // save newBook to database
+  book.save(function(err, book){
+    if (err) {
+      return console.log("save error: " + err);
+    }
+    console.log("saved ", book.title);
+    // send back the book!
+    res.json(book);
+  });
+}
 
-  var bookId = req.params.id;
-
-    // find book in db by id
-    db.Book.findOne({ _id: bookId }).populate('author').exec(function(err, foundBook) {
-        // update the books's attributes
-        foundBook.title = req.body.title;
-        foundBook.author.name = req.body.author;
-
-        // save updated book in db
-        foundBook.save(function(err, savedBook) {
-            console.log("This is before the json",savedBook);
-            res.json(savedBook);
-            
-        });
-    });
-});
 
 // delete book
-app.delete('/api/books/:id', function(req, res) {
+app.delete('/api/books/:id', function (req, res) {
+  // get book id from url params (`req.params`)
+  console.log(req.params)
+  var bookId = req.params.id;
 
-
-  // get Book id from url params (`req.params`)
-  var BookId = req.params.id;
-
-  // find book in db by id and remove
-  db.Book.findOneAndRemove({ _id: BookId }, function(err, deletedBook) {
-      res.json(deletedBook);
+  db.Book.findOneAndRemove({ _id: bookId }, function (err, deletedBook) {
+    res.json(deletedBook);
   });
 });
 
-
-
-app.listen(process.env.PORT || 3000, function() {
-  console.log('Book app listening at http://localhost:3000/');
+app.listen(process.env.PORT || 3000, function () {
+  console.log('Example app listening at http://localhost:3000/');
 });
